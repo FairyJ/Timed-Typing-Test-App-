df51c95817cd36f4de713762f77c409503b27932
Farzaneh Jahani <fairyjahani@farzanehs-mbp.home>
2020-11-13 13:31:30 -0800

    everything is done added function to count error, have to work on last function which is return top 5 record.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Timed-Typing-Test-App-
+# Project Timed Typing Test App
+### Purpose : In this project you are going to create the functionality for a timed typing test app.
+### Requirements : To complete this project you will add to the starter code provided. You may use glitch or any editor that you prefer. For this project you will not need the JQuery library. If you feel the need to use it we can discuss in class. You may edit the structure of the provided HTML and CSS to better fit your preferred style. You may use lecture slides and the past hw’s for sample code but make sure to make it your own.
+### The purpose of this project is to make sure you have an understanding of JavaScript objects,functions, click events, event listeners, and changing DOM elements.
+## Functionality:
+* Run a standard minute/second/hundredths timer
+* Match the text entered with the provided text on the page. Notify user of progress
+* Proper Event listeners for keyboard and button input
+* Start Over button clears input text, changes default notification, and sets clock to zero.
+* Store best times and display the top high scores.
+## What I will be grading on:
+### Implementation of all the functionality. JavaScript, HTML and CSS has proper formatting and comments where appropriate. Web page renders properly in Chrome. Any assets are included in submission.
+### Extra Credit Options:
+* Display a words per minute count.
+* Display the number of errors the person does per test.
+* Add an array of different texts so the tester can switch between different texts to test.
+### Cheating : This project will be an individual project. You can discuss this project with other students. You can explain what needs to be done and give suggestions on how to do it. You cannot share source code. If two projects are submitted which show significant similarity in source code then both students will receive an F for the project. Note a person who gives his code to another student also fails the project (you are facilitating the dishonest actions of another).
---
+++ script.js
@@ -0,0 +1,139 @@
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector(".timer");
+
+var timer;
+//we need to store our min,sec and hundred thousand in different variable so we use array
+var myTimer = [0, 0, 0, 0];
+//create this because we need to use interval out side the startTimer function
+var timeInterval;
+//originally timer is not running
+var timerRunning = false;
+// Add leading zero to numbers 9 or below (purely for aesthetics):
+//when our timer hit 9 we need to change it to 10 here we use string we will add string 0 to our timer. js will treat this string 0 as integer .
+var error = 0;
+//we will have variable for which key was pressed
+
+function leadingZero(t) {
+  if (t <= 9) {
+    t = "0" + t;
+  }
+  return t;
+}
+
+// Run a standard minute/second/hundredths timer:
+
+function startMyTimer() {
+  //we need to keep track of the time so we create some variable
+  //every time startMyTimer runs  we pass each value to leadingZero function and get string as return
+  timer =
+    leadingZero(myTimer[0]) +
+    ":" +
+    leadingZero(myTimer[1]) +
+    ":" +
+    leadingZero(myTimer[2]);
+  //as soon the timer start running each 1000ms should we increase the time
+  theTimer.innerHTML = timer;
+  //we will increase and update the last value in our array
+  myTimer[3]++;
+
+  //define each one of our variable min sec hundred
+  //define min , use floor because we don't need decimal
+  myTimer[0] = Math.floor(myTimer[0] / 100 / 60);
+  //define sec , we will subtract the min*60 from the thousand that will help as son as hit the 60 the value return 0
+  myTimer[1] = Math.floor(myTimer[3] / 100 - myTimer[0] * 60);
+  //define hundred of sec, we subtract sec*100 from thousand of sec will get back to 0 , and alo subtract min *6000 every time min reach 100 will return 0
+  myTimer[2] = Math.floor(myTimer[3] - myTimer[1] * 100 - myTimer[0] * 6000);
+}
+
+// Match the text entered with the provided text on the page:
+//here we will have function which will check the input.
+function checkInput() {
+  // console.dir(testArea);
+  // console.dir(originText);
+
+  //save the user input into a variable
+  let textEntered = testArea.value;
+  //save original text into variable, use substring method which alow use to pull substring and compare them
+  //0 is where to start and how long should be here we set it to the textEnter length
+  let originalTextMatch = originText.substring(0, textEntered.length);
+
+  //check if they are same or not if they are not stop everything
+  if (textEntered == originText) {
+    testWrapper.style.borderColor = "#429890";
+    window.alert(
+      "Great job! you finish it in : " + timer + " you had " + error + " errors"
+    );
+    clearInterval(timeInterval);
+  } else {
+    if (textEntered == originalTextMatch) {
+      testWrapper.style.borderColor = "#65ccf3";
+    } else {
+      testWrapper.style.borderColor = "#FF0000";
+    }
+  }
+  //just check and see what is output
+  // console.log(textEntered);
+}
+
+// Start the timer:
+function startTimer() {
+  //use textLength property to the length and know when to start the timer after enter(keydown) the first word textLength will be 1
+  //used console.dir to see all properties of this object
+  // console.dir(startTimer);
+  let textEnteredLength = testArea.textLength;
+  //just check and see what is output
+  // console.log(textEnteredLength);
+  //check if length is 0 and also timeRunning is false if is true we won't continue the time we will restart it.
+  if (textEnteredLength === 0 && timerRunning === false) {
+    //we will make the timerRunning to true that case if we delete the text and start typing timer won't be running
+    timerRunning = true;
+    //use seInterval to start interval , as soon as the length is 1 this function will start running the function.
+    //run this startMyTimer function every 1000 s.
+    timeInterval = setInterval(startMyTimer, 10);
+  }
+}
+
+// Reset everything:
+function reset() {
+  testArea.value = "";
+  //we need to reset the interval so use clearInterval and pass timerInterval as parameter
+  clearInterval(timeInterval);
+  //we have to set timerInterval variable into null so next time we won't setup new interval with new index number
+  timeInterval = null;
+  //set clock back to 0
+  myTimer = [0, 0, 0, 0];
+  //make timerRunning to false to make it ready for next start point
+  timerRunning = false;
+
+  //we need to set the front to be zero too
+  theTimer.innerHTML = "00:00:00";
+  //change our border color to normal
+  testWrapper.style.borderColor = "gray";
+}
+
+
+
+// Event listeners for keyboard input and the reset button:
+//when user type the first word we want to start our timer so the eventListener type will be keypress
+testArea.addEventListener("keypress", startTimer, false);
+//when we leave the keyboard (keyup) we need to check the words user type
+testArea.addEventListener("keyup", checkInput, false);
+//when click event happen when clear all texted in the testArea
+resetButton.addEventListener("click", reset, false);
+
+testArea.addEventListener("keydown", function() {
+  var key = event.keyCode;
+  // console.dir(key);
+  //if the key pressed cose is 8 or 27 will count as error or mistake that user made so count as error.
+  if (key == 8 || key == 27) {
+    
+    return error++;
+  }
+});
+
+// watched Linkedin learning and learn all of these stuff from there
+// <q></q>
+
